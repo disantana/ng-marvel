87ea2c93d14fe0a22ca010e53814d62984bfce66
Diego de Santana Pereira <diegosant.per@gmail.com>
2020-11-20 01:12:11 -0300

    Feature/set layout (#1)
    * feat: Add a header component
    
    * style: Add a reset css
    
    * style: Add a header style
    
    * style: Add a footer style and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './_shared/header/header.component';
+import { FooterComponent } from './_shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
