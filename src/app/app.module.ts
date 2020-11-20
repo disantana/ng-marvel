242d90f0339d44f22d7446ffc791753024acf202
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2020-11-20 01:13:44 -0300

    feat: Add a characters component and new route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_shared/header/header.component';
 import { FooterComponent } from './_shared/footer/footer.component';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
