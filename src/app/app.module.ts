a70692ef7d01cf8e3d5aed3abec2fc3c57fb5d9b
Diego de Santana Pereira <diegosant.per@gmail.com>
2020-11-20 02:46:27 -0300

    Feature/add character (#2)
    * feat: Add a characters component and new route
    
    * feat: Add angular material
    
    * feat: Add a navigation to character

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_shared/header/header.component';
 import { FooterComponent } from './_shared/footer/footer.component';
+import { CharactersComponent } from './characters/characters.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
