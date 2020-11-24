49384d0e7a0e44cd0d4e57623152ee8aca806f9e
Diego de Santana Pereira <diegosant.per@gmail.com>
2020-11-24 10:50:34 -0300

    feat: Add a movies component (#5)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CharactersComponent} from './characters/characters.component';
 import {ComicsComponent} from './comics/comics.component';
+import {MoviesComponent} from './movies/movies.component';
 
 const routes: Routes = [
   {path: 'characters', component: CharactersComponent},
-  {path: 'comics', component: ComicsComponent}
+  {path: 'comics', component: ComicsComponent},
+  {path: 'movies', component: MoviesComponent}
 ];
 
 @NgModule({
